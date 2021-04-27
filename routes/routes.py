6c6462afd48f3e0dda4cbb902f952abdb887f609
Mazzya <yahyamazouarenna@gmail.com>
2021-04-27 19:51:53 +0200

    Reorganization

--- routes/routes.py
+++ routes/routes.py
@@ -7,6 +7,7 @@ from io import open
 ALLOWED_EXTENSIONS = set(["png", "jpg", "pdf", "gif", "PNG", "jpeg"])
 
 def allowed_file(filename):
+    """ Check the extension of the files to upload """
     return "." in filename and filename.rsplit(".", 1)[1] in ALLOWED_EXTENSIONS
 
 @app.errorhandler(404)
@@ -19,10 +20,12 @@ def home():
 
 @app.route("/about")
 def aboutUs():
+    """ About us page """
     return render_template("about.html")
     
 @app.route("/upload", methods = ["GET", "POST"])
 def uploadFile():
+    """ Main function for uploading files """
     if request.method == "POST":
         if "ourfile" not in request.files:
             return "error1"
@@ -38,12 +41,13 @@ def uploadFile():
 # Return files
 @app.route("/uploads/<filename>")
 def getFile(filename):
+    """ Get files from directory 'content' """
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 
 def showFiles():
-    """ This function shows the files that exist in the directory"""
+    """ This function shows the files that exist in the directory """
     path = "./content/"
     dirs = os.listdir(path)
     files = [file for file in dirs]
